0592e5e616a597fd5c46731264a6269d0dff48d5
Niel Ponciano <nielgp93@gmail.com>
2019-10-06 18:38:15 +0800

    Added support for countring specific target words

--- wordcounter.py
+++ wordcounter.py
@@ -3,27 +3,31 @@
 from collections import defaultdict
 
 DIR_PATH = "./FilesToProcess/"
+INPUT_FILE = "Input.txt"
 WORDS_SEPARATORS_FILE = "WordsSeparators.txt"
 WORDS_TO_COUNT_FILE = "WordsToCount.txt"
 WORDS_TO_IGNORE_FILE = "WordsToIgnore.txt"
 
+with open(DIR_PATH + INPUT_FILE) as fileInput:
+    inputLines = fileInput.readlines()
 with open(DIR_PATH + WORDS_SEPARATORS_FILE) as fileWordsSeparators:
     wordsSeparators = fileWordsSeparators.read()
 with open(DIR_PATH + WORDS_TO_COUNT_FILE) as fileWordsToCount:
-    wordsToCount = fileWordsToCount.readlines()
+    wordsToCount = list(filter(None, fileWordsToCount.read().split("\n")))
 with open(DIR_PATH + WORDS_TO_IGNORE_FILE) as fileWordsToIgnore:
-    wordsToIgnore = fileWordsToIgnore.read().split("\n")
+    wordsToIgnore = list(filter(None, fileWordsToIgnore.read().split("\n")))
 
 wordDict = defaultdict(int)
 SPACE = " "
 
-for line in wordsToCount:
+for line in inputLines:
     for separator in wordsSeparators:
         line = line.replace(separator, SPACE)
 
     for currentWord in line.split():
         currentWordLower = currentWord.casefold()
-        if currentWordLower not in wordsToIgnore:
+        if currentWordLower not in wordsToIgnore and\
+            (len(wordsToCount) == 0 or currentWordLower in wordsToCount):
             wordDict[currentWordLower] += 1
 
 print("\n".join(\
